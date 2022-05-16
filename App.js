eda87b47a193102b6c1cd3d222b6cc527de0c38d
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-05-16 14:30:00 +0200

    new commit3

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          adfjdfjkfkdkjf <Comp/>
+          HALLLOOOOOO <Comp/>
         </p>
 
       </header>
